bc9d85ae68e33ab35c4638f79b431f37f8fe8cf3
Oleksii Palamarchuk <cemper300@gmail.com>
2023-10-15 21:37:24 +0300

    Add calculator for two positive numbers

--- main.cpp
+++ main.cpp
@@ -1,58 +1,77 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
 
-double convertTemperature(int temperature, int convertingFlag)
-{
-    if (convertingFlag == 1)
-    {
-        return static_cast<double>(temperature) * 1.8 + 32.0;
-    }
-    else
-    {
-        return (static_cast<double>(temperature) - 32.0) / 1.8;
-    }
-}
+double add(double value1, double value2);
 
-int main()
-{
-    int convertingFlag;
-    int temperature;
+double subtraction(double value1, double value2);
 
-    std::cout << "Hey there! I can convert temperature for you." << std::endl;
-    std::cout << "Here are your options: \n" << std::endl;
-    std::cout << "0 - Convert from Fahrenheit to Celsius (ºF to ºC)" << std::endl;
-    std::cout << "1 - Convert from Celsius to Fahrenheit (ºC to ºF)\n" << std::endl;
+double multiplication(double value1, double value2);
 
-    std::cin >> convertingFlag;
-    int attempts = 3;
+double division(double value1, double value2);
 
-    while (attempts > 0 && (convertingFlag != 0 && convertingFlag != 1))
-    {
-        std::cout << "You entered an invalid option. Please try again:" << std::endl;
-        std::cin >> convertingFlag;
-        attempts--;
-    }
+std::vector<std::string> splitString(const std::string &line, char symbol);
 
-    if (attempts == 0)
-    {
-        std::cout << "Exceeded maximum attempts. Exiting." << std::endl;
-        return 1;
-    }
+int main() {
 
-    std::cout << "Enter the temperature value: " << std::endl;
-    std::cin >> temperature;
+    std::string line;
+    char symbol;
+    getline(std::cin, line);
 
-    if (convertingFlag == 0)
-    {
-        double result = convertTemperature(temperature, 0);
-        std::cout << "Temperature in Celsius (ºC): " << result << std::endl;
-    }
-    else
-    {
-        double result = convertTemperature(temperature, 1);
-        std::cout << "Temperature in Fahrenheit (ºF): " << result << std::endl;
+    std::vector<std::string> inputParts;
+
+    if (line.find('+') != std::string::npos) {
+        symbol = '+';
+        inputParts = splitString(line, symbol);
+
+        std::cout << add(std::stod(inputParts[0]), std::stod(inputParts[1]));
+    } else if (line.find('-') != std::string::npos) {
+        symbol = '-';
+        inputParts = splitString(line, symbol);
+
+        std::cout << subtraction(std::stod(inputParts[0]), std::stod(inputParts[1]));
+    } else if (line.find('*') != std::string::npos) {
+        symbol = '*';
+        inputParts = splitString(line, symbol);
+
+        std::cout << multiplication(std::stod(inputParts[0]), std::stod(inputParts[1]));
+    } else if (line.find('/') != std::string::npos) {
+        symbol = '/';
+        inputParts = splitString(line, symbol);
+
+        std::cout << division(std::stod(inputParts[0]), std::stod(inputParts[1]));
     }
 
     return 0;
+}
+
+double add(double value1, double value2) {
+    return value1 + value2;
+}
+
+double subtraction(double value1, double value2) {
+    return value1 - value2;
+}
+
+double multiplication(double value1, double value2) {
+    return value1 * value2;
+}
+
+double division(double value1, double value2) {
+    return value1 / value2;
+}
+
+std::vector<std::string> splitString(const std::string &line, char symbol) {
+    std::vector<std::string> parts;
+    std::istringstream ss(line);
+    std::string token;
+
+    while (std::getline(ss, token, symbol)) {
+        parts.push_back(token);
+    }
+
+    return parts;
 }
 
 //Hello World with User Input:
@@ -105,6 +124,60 @@ int main()
 
 //Temperature Conversion:
 //Build a program that converts temperatures between Fahrenheit and Celsius. Ask the user for a temperature in one unit and display the equivalent temperature in the other unit.
+//double convertTemperature(int temperature, int convertingFlag)
+//{
+//    if (convertingFlag == 1)
+//    {
+//        return static_cast<double>(temperature) * 1.8 + 32.0;
+//    }
+//    else
+//    {
+//        return (static_cast<double>(temperature) - 32.0) / 1.8;
+//    }
+//}
+//
+//int main()
+//{
+//    int convertingFlag;
+//    int temperature;
+//
+//    std::cout << "Hey there! I can convert temperature for you." << std::endl;
+//    std::cout << "Here are your options: \n" << std::endl;
+//    std::cout << "0 - Convert from Fahrenheit to Celsius (ºF to ºC)" << std::endl;
+//    std::cout << "1 - Convert from Celsius to Fahrenheit (ºC to ºF)\n" << std::endl;
+//
+//    std::cin >> convertingFlag;
+//    int attempts = 3;
+//
+//    while (attempts > 0 && (convertingFlag != 0 && convertingFlag != 1))
+//    {
+//        std::cout << "You entered an invalid option. Please try again:" << std::endl;
+//        std::cin >> convertingFlag;
+//        attempts--;
+//    }
+//
+//    if (attempts == 0)
+//    {
+//        std::cout << "Exceeded maximum attempts. Exiting." << std::endl;
+//        return 1;
+//    }
+//
+//    std::cout << "Enter the temperature value: " << std::endl;
+//    std::cin >> temperature;
+//
+//    if (convertingFlag == 0)
+//    {
+//        double result = convertTemperature(temperature, 0);
+//        std::cout << "Temperature in Celsius (ºC): " << result << std::endl;
+//    }
+//    else
+//    {
+//        double result = convertTemperature(temperature, 1);
+//        std::cout << "Temperature in Fahrenheit (ºF): " << result << std::endl;
+//    }
+//
+//    return 0;
+//}
 //
 //Simple Calculator:
 //Implement a basic calculator that can perform addition, subtraction, multiplication, and division. Take input from the user for two numbers and an operator (+, -, *, /) and display the result.
