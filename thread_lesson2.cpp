1d43b777323ee4e10d5de596dbf044ddb7b3de0c
Oleksii Palamarchuk <cemper300@gmail.com>
2023-10-24 20:52:41 +0300

    catch race condition

--- thread_lesson2.cpp
+++ thread_lesson2.cpp
@@ -6,34 +6,21 @@ int myValue = 0;
 std::mutex m;
 
 void incr(){
-    // m.lock();
-    ++myValue;
-    // m.unlock();
+ // m.lock();
+ for (int i = 0; i < 100000; ++i) {
+        myValue++;
+    }
+ // m.unlock();
 }
 
 int main() {
-    int step = 1;
-    while (step > 0) {
     std::thread t1(incr);
     std::thread t2(incr);
-    std::thread t3(incr);
-    std::thread t4(incr);
-    std::thread t5(incr);
-    std::thread t6(incr);
 
     t1.join();
     t2.join();
-    t3.join();
-    t4.join();
-    t5.join();
-    t6.join();
     
-    std::cout << step << ": "<< myValue << std::endl;
-    myValue = 0;
-    step++;
-    if (myValue == 5) {
-        step = 0;
-    }
-    }
+    std::cout << "Value: "<< myValue << std::endl;
+    
     return 0;
 }
